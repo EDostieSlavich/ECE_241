b2f8472966a881ed4c8f2313dc923f149f774848
EDostieSlavich <edostie@umass.edu>
2021-09-03 11:29:41 -0400

    Initial commit

---
+++ venv/question1.py
@@ -0,0 +1,8 @@
+class HW1Q1():
+    def __init__(self, second=60*60*24, minute=60*24, hour=24, day=1):
+        self.second = second
+        self.minute = minute
+        self.hour = hour
+        self.day = day
+
+        
